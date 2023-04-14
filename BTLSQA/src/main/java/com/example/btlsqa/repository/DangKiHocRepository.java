da364d2bcf213de1d82d1d6b7b8eb66806ede6fd
ThanhLVlucifer <levanthanhptiter@gmail.com>
2023-04-14 07:58:03 +0700

    update_dk

--- BTLSQA/src/main/java/com/example/btlsqa/repository/DangKiHocRepository.java
+++ BTLSQA/src/main/java/com/example/btlsqa/repository/DangKiHocRepository.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @Repository
 public interface DangKiHocRepository extends JpaRepository<DangKiHoc, Integer> {
-    @Query("SELECT dk.lopHocPhan.monHoc.id FROM DangKiHoc dk WHERE dk.sinhVien.id = :sinhVienId")
+    @Query("SELECT dk.lopHocPhan.monHoc.id FROM DangKiHoc dk WHERE dk.sinhVien.id = :sinhVienId AND dk.diemTongKet >= 1.6")
     List<String> findMonHocBySinhVienId(@Param("sinhVienId") Integer sinhVienId);
     @Query("SELECT dk FROM DangKiHoc dk WHERE dk.sinhVien.id = :sinhVienId AND dk.soLanHoc=0")
     List<DangKiHoc> findAllLopHocPhanDangDangKiBySinhVienId(@Param("sinhVienId") Integer sinhVienId);
